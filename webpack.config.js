03c727f1947dc9c58b396a15806b491c6adee058
abelmuro <abelmuro93@gmail.com>
2024-05-03 12:59:30 -0700

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
     plugins: [                      
         new HtmlWebpackPlugin({               //this plugin will help us generate the production html file in our /dist
             filename: 'index.html',           //our production html file will be named index.html
+            favicon: './public/PWA/icons/favicon-32x32.png',
             template: './src/index.html'      //this is a template for our production html file, we are defining how the html will look like before we make our production html file
         }),
        new CopyWebpackPlugin({
